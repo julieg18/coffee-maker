82b04e1efca7b4b813fe572e7b21906bd3c8dc85
julieg18 <juliannagal18@gmail.com>
2020-03-27 07:33:52 -0500

    set up project

---
+++ src/scripts/finish.js
@@ -0,0 +1,1 @@
+import '../pages/finish.css';
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: './src/scripts/index.js',
+    main: './src/scripts/main.js',
+    finish: './src/scripts/finish.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    port: 8080,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      chunks: ['index'],
+      filename: 'index.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/main.html',
+      chunks: ['main'],
+      filename: 'main.html',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/finish.html',
+      chunks: ['finish'],
+      filename: 'finish.html',
+    }),
+  ],
+};
