e86a992abbbfc54c652cff19b23383e1f6ad027d
julieg18 <juliannagal18@gmail.com>
2020-04-07 06:00:57 -0500

    feat: complete coffee sizes and flavorings

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
+      {
+        test: /\.svg$/,
+        use: 'file-loader',
+      },
     ],
   },
   plugins: [
