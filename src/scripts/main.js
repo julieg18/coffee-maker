913cf63edee1a00c5455d18d73ce25c8568fbd8d
julieg18 <juliannagal18@gmail.com>
2020-04-24 09:08:45 -0500

    feat: create working form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,21 @@
 import '../main.html';
 import '../pages/main.css';
+
+const labels = document.querySelectorAll('label');
+
+function clickCorrespondingInput(e) {
+  const id = this.getAttribute('for');
+  if (
+    this === e.target ||
+    e.target.classList.contains('customization__choice-btn')
+  ) {
+    document.querySelector(`#${id}`).click();
+  } else {
+    e.stopPropagation();
+  }
+}
+
+labels.forEach((label) => {
+  label.addEventListener('click', clickCorrespondingInput);
+  return label;
+});
